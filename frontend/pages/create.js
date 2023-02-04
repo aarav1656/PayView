de875e0e1afa80ccb032ee1de5b489541dfe4c63
ShivaliLN <shivali8910@gmail.com>
2023-02-04 17:25:08 -0500

    callContract

--- frontend/pages/create.js
+++ frontend/pages/create.js
@@ -2,26 +2,23 @@ import { useAccount } from "wagmi";
 import { useEffect } from "react";
 import React, { useState } from "react";
 import { FormControl, FormLabel, Input, Button, Box, Flex} from "@chakra-ui/react";
+import { create, urlSource } from 'ipfs-http-client'
+import { Buffer } from 'buffer'
+import {ABI} from '../utils/factory_contract';
+import {ADDRESS} from '../utils/factory_contract';
+const { providers, Contract, getSigner, ethers   } = require('ethers');
 
+/* configure Infura auth settings */
+const projectId = "<INPUT>"
+const projectSecret ="<INPUT>"
+const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+const ipfs = create({ host: 'ipfs.infura.io', port: '5001', protocol: 'https', headers: {
+  authorization: auth,
+}, })
 
 function Create() {
-  /*
-  const { address } = useAccount();
-
-  console.log(address);
-
-  useEffect(() => {
-    canUpload();
-  }, []);
-
-  const canUpload = () => {
-    if (!address) {
-      return <>You need to connect the wallet before you can upload</>;
-    }
-  };
-
-  return <div>Create </div>;
-  */
+  const [values, setValues] = useState({ message: ""});	
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -37,7 +34,57 @@ function Create() {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Form data: ", formData);
+    // Connect to a Hyperspace (Filecoin) node
+    const provider = new providers.JsonRpcProvider('https://api.hyperspace.node.glif.io/rpc/v1');
+
+    // The address of the smart contract
+    const contractAddress = ADDRESS;
+
+    // Create an instance of the Contract object
+    const contract = new Contract(contractAddress, ABI, provider);
+
+    // Call the READ function
+    contract.getPayViewContract(1).then((result) => {
+    console.log(`The result of the function call is: ${result}`);
+  });
+
+    // Call the WRITE function
+    const provider2 = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider2.getSigner();
+
+    const _tokenName = document.getElementById("nftname").value
+    const _symbol= document.getElementById("symbol").value
+    const _uri= document.getElementById("jsonuri").value
+    const _amount=document.getElementById("amount").value
+    const _vid_cid=document.getElementById("videocid").value
+
+    contract.connect(signer).createNewPayView(_tokenName, _symbol, _uri, _amount, _vid_cid).then((result) => {
+      console.log(`The result of the function call is: ${result}`);
+  
+    });    
+
   };
+
+  async function uploadIPFS(){   
+    // Image Upload First
+    const fileInput = document.getElementById("image")
+    const data = fileInput.files[0]
+    const added = await ipfs.add(data)
+    const url = `https://infura-ipfs.io/ipfs/${added.path}`
+    console.log("IPFS URI: ", url)
+
+    //Create JSON 
+    const object = {
+      "name" : document.getElementById("name").value,
+      "description": document.getElementById("description").value,
+      "image": url,
+    }
+    const file = Buffer.from(JSON.stringify(object));
+    const uploaded = await ipfs.add(file)
+    const url2 = `https://infura-ipfs.io/ipfs/${uploaded.path}`
+    console.log("IPFS URI: ", url2)
+    setValues({ ...values, message: url2 })
+  }
 
   return (
    
@@ -45,35 +92,61 @@ function Create() {
       <Box maxW="800px" borderWidth="2px" rounded="lg" overflow="hidden" boxShadow="md">
         <form>
         <FormControl mt={4} w="100%">
+            <FormLabel htmlFor="nftname" color="white">
+              Name
+            </FormLabel>
+            <Flex >
+            <Input type="text" id="nftname" color="white" w='150%' /></Flex>
+          </FormControl>
+          <FormControl mt={4} w="100%">
+            <FormLabel htmlFor="symbol" color="white">
+              Symbol
+            </FormLabel>
+            <Input type="text" id="symbol" color="white" />
+          </FormControl>
+          <FormControl mt={4} w="100%">
+            <FormLabel htmlFor="jsonuri" color="white">
+              NFT_JSON_URI
+            </FormLabel>
+            <Input type="text" id="jsonuri" color="white" />
+          </FormControl>
+          <FormLabel htmlFor="name" color="Yellow">
+          OR, Enter NFT JSON Information below:
+          </FormLabel>
+          <Box bg='gray.800'minH="25vh" w='100%' p={4} color='white' alignItems="center">
+            <Box maxW="500px" borderWidth="2px" rounded="lg" overflow="hidden" boxShadow="md">
             <FormLabel htmlFor="name" color="white">
-              Description (Token Name)
+              Name
             </FormLabel>
             <Flex >
             <Input type="text" id="name" color="white" w='150%' /></Flex>
-          </FormControl>
-          <FormControl mt={4} w="100%">
-            <FormLabel htmlFor="name" color="white">
-              Symbol
+            <FormLabel htmlFor="description" color="white">
+              Description
             </FormLabel>
-            <Input type="text" id="name" color="white" />
-          </FormControl>
+            <Flex >
+            <Input type="text" id="description" color="white" w='150%' /></Flex>
+            <FormControl mt={4}>
+        <FormLabel htmlFor="file">Upload Image</FormLabel>
+        <Input type="file" id="image" />
+      </FormControl>
+      <FormLabel htmlFor="name" color="Yellow">
+      Your IPFS Link: {values.message} </FormLabel>
+      <Button mt={4} variantColor="teal" onClick={uploadIPFS} color="black">
+            Create JSON & Upload to IPFS
+          </Button>
+             </Box>
+          </Box>
           <FormControl mt={4} w="100%">
-            <FormLabel htmlFor="name" color="white">
-              URI
-            </FormLabel>
-            <Input type="text" id="name" color="white" />
-          </FormControl>
-          <FormControl mt={4} w="100%">
-            <FormLabel htmlFor="name" color="white">
+            <FormLabel htmlFor="amount" color="white">
               Amount per NFT
             </FormLabel>
-            <Input type="text" id="name" color="white" />
+            <Input type="text" id="amount" color="white" />
           </FormControl>
           <FormControl mt={4} w="100%">
-            <FormLabel htmlFor="name" color="white">
+            <FormLabel htmlFor="videocid" color="white">
               Enter your video CID
             </FormLabel>
-            <Input type="text" id="name" color="white" />
+            <Input type="text" id="videocid" color="white" />
           </FormControl>
           <FormLabel htmlFor="name" color="white">
           OR
@@ -82,7 +155,7 @@ function Create() {
         <FormLabel htmlFor="file">Upload File</FormLabel>
         <Input type="file" id="file" />
       </FormControl>
-          <Button mt={4} variantColor="teal" type="submit" color="black">
+          <Button mt={4} variantColor="teal" onClick={handleSubmit} color="black">
             Submit
           </Button>
         
